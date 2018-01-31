9d079b9ae9edca87769a22de2ca2677306391289
Gabe Tiller <gabectiller@gmail.com>
2018-01-30 23:56:18 -0500

    get_me complete

--- lib/kele.rb
+++ lib/kele.rb
@@ -6,18 +6,24 @@ class Kele
   include HTTParty
   format :json
 
-
-  base_uri 'https://www.bloc.io/api/v1'
+  # base_uri 'https://www.bloc.io/api/v1'
 
   def initialize(email, password)
-    @options = {query: {email: email, password: password}}
-    response = self.class.post("/sessions", @options)
-    @auth_token = response["auth_token"]
-    if response && response["auth_token"]
-      puts "This is your authorization token: #{@auth_token}"
-    else
-      puts "invalid user input"
-    end
+    @options = { query: { email: email, password: password } }
+    response = self.class.post(api_url("sessions"), @options)
+      raise "Invalid email or password" if response.code == 404
+      @auth_token = response["auth_token"]
+  end
+
+  def get_me
+    response = self.class.get(api_url("users/me"), headers: { "authorization" => @auth_token })
+    @user_data = JSON.parse(response.body)
+  end
+
+  private
+
+  def api_url(endpoint)
+    "https://www.bloc.io/api/v1/#{endpoint}"
   end
 
 end
